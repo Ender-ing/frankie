999ac561d4c66d45862776292bfb95c9ca18f229
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-11-28 05:29:20 +0200

    Update build.bat

--- transpiler/tools/antlr4-build/build.bat
+++ transpiler/tools/antlr4-build/build.bat
@@ -5,6 +5,9 @@ setlocal enabledelayedexpansion
 set input_folder=.\..\..\source\ANTLR4
 set output_folder=.\..\..\generated\ANTLR4
 
+:: arguments
+set antlr4_build=java org.antlr.v4.Tool -Werror
+
 :: Empty the output directory
 del /q /s "%output_folder%\*.cpp"
 del /q /s "%output_folder%\*.h"
@@ -15,9 +18,9 @@ del /q /s "%output_folder%\*.tokens"
 mkdir %output_folder%
 
 :: Process the lexer
-java org.antlr.v4.Tool %input_folder%\PolarFrankieLexer.g4
+%antlr4_build% %input_folder%\PolarFrankieLexer.g4
 :: Process the parser
-java org.antlr.v4.Tool %input_folder%\PolarFrankieParser.g4
+%antlr4_build% %input_folder%\PolarFrankieParser.g4
 
 :: Move the files to the generated files directory
 move "%input_folder%\*.cpp" "%output_folder%\"
