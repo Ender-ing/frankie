9762311f509676b16466b31514e3fb811bf861d0
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-11-28 03:51:13 +0200

    Update build command

--- transpiler/tools/antlr4-build/build.bat
+++ transpiler/tools/antlr4-build/build.bat
@@ -1,4 +1,4 @@
 :: Process the lexer
-java org.antlr.v4.Tool .\..\..\source\ANTLR4\PolarFrankieLexer
+java org.antlr.v4.Tool .\..\..\source\ANTLR4\PolarFrankieLexer.g4 -o .\..\..\generated\ANTLR4
 :: Process the parser
-java org.antlr.v4.Tool .\..\..\source\ANTLR4\PolarFrankieParser
+java org.antlr.v4.Tool .\..\..\source\ANTLR4\PolarFrankieParser.g4 -o .\..\..\generated\ANTLR4
