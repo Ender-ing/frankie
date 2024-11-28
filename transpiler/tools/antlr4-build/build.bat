bc9479ff629ec6d65f4e3c7e27d9260ed856d241
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-11-28 04:09:54 +0200

    Update build.bat

--- transpiler/tools/antlr4-build/build.bat
+++ transpiler/tools/antlr4-build/build.bat
@@ -1,4 +1,17 @@
+:: Input and output
+set input_folder=.\..\..\source\ANTLR4
+set output_folder=.\..\..\generated\ANTLR4
+
+:: Make the output
+mkdir %output_folder%
+
 :: Process the lexer
-java org.antlr.v4.Tool -O .\..\..\generated\ANTLR4 .\..\..\source\ANTLR4\PolarFrankieLexer.g4
+java org.antlr.v4.Tool %input_folder%\PolarFrankieLexer.g4
 :: Process the parser
-java org.antlr.v4.Tool -O .\..\..\generated\ANTLR4 .\..\..\source\ANTLR4\PolarFrankieParser.g4
+java org.antlr.v4.Tool %input_folder%\PolarFrankieParser.g4
+
+:: Move the files to the generated files directory
+
+for %%f in (%input_folder%\*.your_extension) do (
+    move "%%f" "%output_folder%\%%f"
+)
