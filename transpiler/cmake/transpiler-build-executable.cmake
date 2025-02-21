c4da12c55bcc92f6589e249b9aa96c33ab1ec4f2
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-02-21 20:49:21 +0200

    Update transpiler-build-executable.cmake

--- transpiler/cmake/transpiler-build-executable.cmake
+++ transpiler/cmake/transpiler-build-executable.cmake
@@ -2,4 +2,4 @@
 add_executable(FrankieTranspiler ${FRANKIE_MAIN_CPP_PATH}
                ${ANTLR_FrankieGrammarLexer_CXX_OUTPUTS}
                ${ANTLR_FrankieGrammarParser_CXX_OUTPUTS})
-target_link_libraries(demo antlr4_static)
+target_link_libraries(FrankieTranspiler antlr4_static)
