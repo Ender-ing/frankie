954e8353d470d9d319fc6f0e7bcabb3e26617a0d
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-02-21 20:58:38 +0200

    Add ANTLR4 files generation

--- transpiler/cmake/transpiler-build-executable.cmake
+++ transpiler/cmake/transpiler-build-executable.cmake
@@ -1,4 +1,7 @@
-# add generated grammar to binary target
+# Generate ANTLR4 grammar C++ files
+include(${FRANKIE_CMAKE_DIR}/generateANTLR4Files.cmake)
+
+# Add generated grammar to binary target
 add_executable(FrankieTranspiler ${FRANKIE_MAIN_CPP_PATH}
                ${ANTLR_FrankieGrammarLexer_CXX_OUTPUTS}
                ${ANTLR_FrankieGrammarParser_CXX_OUTPUTS})
