d50293300ed69efa1140cc8d2393bb176499b47d
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-02-22 19:30:49 +0200

    Add `cmake` frankie files tests

---
+++ transpiler/cmake/test-executable.cmake
@@ -0,0 +1,19 @@
+# Define testing function
+function(frankie_file_test test_name file_path)
+    add_test(NAME FrankieFileTest__${test_name}_find COMMAND ${CMAKE_COMMAND} -E file EXISTS ${file_path})
+    add_test(NAME FrankieFileTest__${test_name}_execute COMMAND FrankieTranspiler ${file_path})
+endfunction()
+
+# Include all .test.cmake files in the tests directory
+# Search for all .test.cmake files recursively
+file(GLOB_RECURSE TEST_CMAKE_FILES ${FRANKIE_TESTS_DIR}/*.test.cmake)
+# Include each .test.cmake file
+foreach(TEST_CMAKE_FILE ${TEST_CMAKE_FILES})
+  include("${TEST_CMAKE_FILE}")
+endforeach()
+
+# Example: Adding a test that runs a script
+# add_test(NAME ScriptTest COMMAND ${CMAKE_COMMAND} -E echo "Running script test")
+
+# Example: Adding a test that checks for file existence
+# add_test(NAME FileTest COMMAND ${CMAKE_COMMAND} -E file EXISTS my_file.txt)
---
+++ transpiler/tests/types/basic/basic.test.cmake
@@ -0,0 +1,4 @@
+# Run the basic test function
+frankie_file_test(Types_Basic_Booleans ${CMAKE_CURRENT_LIST_FILE}/booleans.frankie)
+frankie_file_test(Types_Basic_Numbers ${CMAKE_CURRENT_LIST_FILE}/numbers.frankie)
+frankie_file_test(Types_Basic_Strings ${CMAKE_CURRENT_LIST_FILE}/strings.frankie)
