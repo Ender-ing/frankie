c68756deb37aebcdbc756425f4b9d44c6095a4ef
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-02 22:48:08 +0200

    Mark libraries as dependencies

--- transpiler/cmake/build-executable.cmake
+++ transpiler/cmake/build-executable.cmake
@@ -20,4 +20,6 @@ foreach(LIB ${PROJECT_STATIC_LIBRARIES})
     target_link_libraries(${LIB} PUBLIC fmt::fmt)
     # Add the library
     target_link_libraries(FrankieTranspiler ${LIB})
+    # Mark the library as a dependency of the executable
+    add_dependencies(FrankieTranspiler ${LIB})
 endforeach()
