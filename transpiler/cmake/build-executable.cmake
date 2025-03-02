75830ea72025607351ac649f47de15bd6ec8225a
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-03 00:38:16 +0200

    Update build-executable.cmake

--- transpiler/cmake/build-executable.cmake
+++ transpiler/cmake/build-executable.cmake
@@ -16,7 +16,6 @@ set_property(TARGET fmt::fmt PROPERTY POSITION_INDEPENDENT_CODE ON)
 foreach(LIB ${PROJECT_LIBRARIES})
     # Link other public libraries to the library
     target_link_libraries(${LIB} fmt::fmt)
-    set_property(TARGET ${LIB} PROPERTY POSITION_INDEPENDENT_CODE ON)
     # Add the library
     target_link_libraries(FrankieTranspiler ${LIB})
     # Mark the library as a dependency of the executable
