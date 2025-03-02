f40d34e49a30a736f886b3ca0ab42a3cf872a269
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-03 01:21:18 +0200

    Update cmake command

--- transpiler/cmake/build-output.cmake
+++ transpiler/cmake/build-output.cmake
@@ -10,15 +10,3 @@ set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG ${FRANKIE_BINARY_DIR}/bin)
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE ${FRANKIE_BINARY_DIR}/bin)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${FRANKIE_BINARY_DIR}/lib)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${FRANKIE_BINARY_DIR}/lib)
-
-# Move MSVC files into the main folder
-#add_custom_command(TARGET FrankieTranspiler
-#                   POST_BUILD
-#                   COMMAND ${CMAKE_COMMAND}
-#                           -E move ./Release/* .
-#                   WORKING_DIRECTORY ${FRANKIE_BINARY_DIR})
-#                   add_custom_command(TARGET FrankieTranspiler
-#                   POST_BUILD
-#                   COMMAND ${CMAKE_COMMAND}
-#                           -E move ./Debug/* .
-#                   WORKING_DIRECTORY ${FRANKIE_BINARY_DIR})
