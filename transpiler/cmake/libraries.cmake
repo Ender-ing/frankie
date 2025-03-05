3d07f3b1043f3f915d169630c88e0f8ae4a54394
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-05 18:29:23 +0200

    Update libraries.cmake

--- transpiler/cmake/libraries.cmake
+++ transpiler/cmake/libraries.cmake
@@ -7,7 +7,7 @@ add_library(root_library SHARED
     ${FRANKIE_SOURCE_DIR}/config.cpp
 )
 # Expose library exports
-target_compile_definitions(root_library PRIVATE BUILDING_DYNAMIC_LIBRARY)
+target_compile_definitions(root_library PUBLIC BUILDING_DYNAMIC_LIBRARY)
 
 # Create a library from /comms
 add_library(comms_library SHARED
@@ -15,7 +15,7 @@ add_library(comms_library SHARED
     ${FRANKIE_SOURCE_DIR}/comms/comms.cpp
 )
 # Expose library exports
-target_compile_definitions(comms_library PRIVATE BUILDING_DYNAMIC_LIBRARY)
+target_compile_definitions(comms_library PUBLIC BUILDING_DYNAMIC_LIBRARY)
 # Link other libraries to the library
 add_dependencies(comms_library fmt::fmt)
 target_link_libraries(comms_library PUBLIC fmt::fmt)
@@ -25,7 +25,7 @@ add_library(common_library SHARED
     ${FRANKIE_SOURCE_DIR}/common/files.cpp
 )
 # Expose library exports
-target_compile_definitions(common_library PRIVATE BUILDING_DYNAMIC_LIBRARY)
+target_compile_definitions(common_library PUBLIC BUILDING_DYNAMIC_LIBRARY)
 
 # Create a library from /parser
 add_library(parser_library SHARED
@@ -34,7 +34,7 @@ add_library(parser_library SHARED
     ${FRANKIE_SOURCE_DIR}/parser/parser.cpp
 )
 # Expose library exports
-target_compile_definitions(parser_library PRIVATE BUILDING_DYNAMIC_LIBRARY)
+target_compile_definitions(parser_library PUBLIC BUILDING_DYNAMIC_LIBRARY)
 # ANTLR4
 add_dependencies(parser_library antlr4_shared)
 target_link_libraries(parser_library PUBLIC antlr4_shared)
