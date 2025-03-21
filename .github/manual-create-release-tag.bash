e4e1128db924b35261a15f65a10b9fc103374c09
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-21 19:26:18 +0200

    Update version syntax

--- .github/manual-create-release-tag.bash
+++ .github/manual-create-release-tag.bash
@@ -1,14 +1,14 @@
 # Alpha
 # (Used to release very early and untested binaries)
-git tag -a "v0.0.0-alpha-000" -m "Alpha-000 v0.0.0"
+git tag -a "v0.0.0-alpha.000" -m "Alpha #000 v0.0.0"
 
 # Beta
 # (Used to release experimental binaries)
-git tag -a "v0.0.0-beta-00" -m "Beta-00 v0.0.0"
+git tag -a "v0.0.0-beta.00" -m "Beta #00 v0.0.0"
 
 # Prerelease
 # (Used to release polished binaries)
-git tag -a "v0.0.0-prerelease-00" -m "Prerelease-00 v0.0.0"
+git tag -a "v0.0.0-prerelease.00" -m "Prerelease #00 v0.0.0"
 
 # Release
 # (Used to release the final release)
