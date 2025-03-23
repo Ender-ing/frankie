9afe468de06fa752c2a28b7d113fb866eea9dc49
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-24 00:13:01 +0200

    Add `report.cpp`

---
+++ transpiler/source/comms/CLI/report.cpp
@@ -0,0 +1,16 @@
+/**
+ * @brief
+ * Handle CLI report displays
+**/
+
+namespace Comms {
+    namespace CLI {
+        // Handle reports CLI outputs
+        namespace Reports {
+            // Print report details
+            void print(){
+                //
+            }
+        }
+    }
+}
