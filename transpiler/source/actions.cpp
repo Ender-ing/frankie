1eabd6fca60b5a023a1776bd5495487bfa367076
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-24 04:46:18 +0200

    Add action input description

--- transpiler/source/actions.cpp
+++ transpiler/source/actions.cpp
@@ -33,8 +33,35 @@ namespace Base {
                 "Forcefully feed all output (status, warnings, or errors) into the normal standard output stream!"
             },*/
             DEFINE_ACTION(
+                "p", "protocol",
+                "Set the data output protocol.",
+                "c (console, default) | s (language server)",
+                {
+                    // Get the next argument and save it!
+                    std::string protocolText;
+                    bool success = getNextArg(Base::InitialConfigs::protocol, true);
+
+                    // Check if the action was successful!
+                    if (!success) {
+                        // Missing input argument!
+                        // PRINT AN ERROR!
+                        ACTION_FAILURE;
+                    }
+
+                    // Check if the protocol value is valid!
+                    if(!(Base::InitialConfigs::protocol == "c" || Base::InitialConfigs::protocol == "s")) {
+                        // Incorrect input value!
+                        // PRINT AN ERROR!
+                        ACTION_FAILURE;
+                    }
+
+                    ACTION_SUCCESS;
+                }
+            ),
+            DEFINE_ACTION(
                 "i", "input",
                 "Set a path for the main user input file.",
+                "<path>",
                 {
                     // Get the next argument and save it!
                     bool success = getNextArg(Base::InitialConfigs::mainPath, true);
@@ -46,12 +73,15 @@ namespace Base {
                         ACTION_FAILURE;
                     }
 
+                    // Check if the file exists!
+
                     ACTION_SUCCESS;
                 }
             ),
             DEFINE_ACTION(
                 "dbg-antlr-print", "debug-parser-antlr-print-test",
                 "Print the parser's tokens list and initial parser output.",
+                "",
                 {
                     // Enable the test
                     InitialConfigs::Debug::Parser::activateBasicPrintTest = true;
