a96c82644e6951130f8690345b2e897465777a42
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-24 04:47:03 +0200

    Update `Comms::CLI:Reports`

--- transpiler/source/comms/CLI/report.cpp
+++ transpiler/source/comms/CLI/report.cpp
@@ -3,13 +3,48 @@
  * Handle CLI report displays
 **/
 
+#include "report.hpp"
+
 namespace Comms {
     namespace CLI {
         // Handle reports CLI outputs
         namespace Reports {
+            // Sanitise strings for console output
+            static void sanitize(std::string &str) {
+                // Symbols to replace ({fmt})
+                static std::array<std::array<std::string, 2>, 2> symbols = {
+                    std::array<std::string, 2>{"{", "{{"},
+                    std::array<std::string, 2>{"}", "}}"}
+                };
+
+                // Replace all symbols
+                for (const auto& pair : symbols) {
+                    size_t pos = 0;
+                    while ((pos = str.find(pair[0], pos)) != std::string::npos) {
+                        str.replace(pos, 1, pair[1]); // Replace 1 char at pos with newString
+                        pos += pair[1].length(); // Move past the replaced string
+                    }
+                }
+            }
+
             // Print report details
-            void print(){
-                //
+            void print() {
+                // TMP
+                uint32_t color;
+                if (IndividualReport::type == CRITICAL_REPORT) {
+                    color = Color::red;
+                } else if (IndividualReport::type == WARNING_REPORT) {
+                    color = Color::yellow;
+                } else {
+                    color = Color::white;
+                }
+
+                // TMP
+                for (auto& data : IndividualReport::messageBodyData) {
+                    sanitize(data);
+                    std::cerr << Comms::CLI::format(data, color) << std::flush;
+                }
+                std::cerr << std::endl;
             }
         }
     }
