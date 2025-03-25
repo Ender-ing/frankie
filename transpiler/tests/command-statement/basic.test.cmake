17343be9979a8db3a801615ad877852117c43c54
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-25 03:46:07 +0200

    Add more tests

---
+++ transpiler/tests/command-statement/basic.test.cmake
@@ -0,0 +1,2 @@
+# Run the basic test function
+frankie_file_test(Commands_Statement ${CMAKE_CURRENT_LIST_DIR}/statement.frankie)
---
+++ transpiler/tests/grouping/basic.test.cmake
@@ -0,0 +1,4 @@
+# Run the basic test function
+frankie_file_test(Grouping_Expressions ${CMAKE_CURRENT_LIST_DIR}/expressions-groups.frankie)
+frankie_file_test(Grouping_Capture_Output ${CMAKE_CURRENT_LIST_DIR}/output-capture.frankie)
+frankie_file_test(Grouping_Capture_Script ${CMAKE_CURRENT_LIST_DIR}/script-capture.frankie)
