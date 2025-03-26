735ae865030fe90c39ec776d2817516157af2bd1
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-26 20:55:34 +0200

    Update `manifest.ini` data

--- transpiler/cmake/build-output.cmake
+++ transpiler/cmake/build-output.cmake
@@ -68,3 +68,31 @@ set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG ${FRANKIE_BINARY_DIR}/Debug/${FRANKIE_B
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE ${FRANKIE_BINARY_DIR}/Release/${FRANKIE_BINARY_PLATFORM}/bin)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${FRANKIE_BINARY_DIR}/Debug/${FRANKIE_BINARY_PLATFORM}/lib)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${FRANKIE_BINARY_DIR}/Release/${FRANKIE_BINARY_PLATFORM}/lib)
+
+# Add a .ini value reader
+function(get_ini_value INI_FILE INI_SECTION KEY OUTPUT_VARIABLE)
+    # Get file contents
+    file(READ ${INI_FILE} INI_CONTENTS)
+    string(REGEX REPLACE "\r" "" INI_CONTENTS "${INI_CONTENTS}")
+
+    # Get the required section
+    string(REGEX MATCH "(\\[${INI_SECTION}\\]\n)(([^\\[]*)\n)*\\[?" MATCHED_SECTION "${INI_CONTENTS}")
+
+    # Look for the key
+    string(REGEX MATCH "(^|\n)(${KEY}\\=[^\n]*)" MATCHED_VALUE "${MATCHED_SECTION}")
+    # Only get the value
+    # (?<=(\[FrankieTranspiler\]\n))^VERSION=([^\n]*) # Doesn't work if the value is not directly after the section's name
+    string(REGEX REPLACE "(^|\n)${KEY}\\=[\\s]*(\"(.*)\"|([\\d]+))[\\s]*" "\\3" VALUE "${MATCHED_VALUE}")
+    string(STRIP "${VALUE}" VALUE) # Remove extra whitespace (for bad string values)
+
+    # Check for value inheritance
+    string(REGEX MATCH "^INHERIT:.+" INIT_INHERIT "${VALUE}")
+    if(INIT_INHERIT)
+        string(REGEX REPLACE "INHERIT:(.*)" "\\1" INIT_INHERIT "${INIT_INHERIT}")
+        # Get value from INIT_INHERIT's section
+        get_ini_value(${INI_FILE} ${INIT_INHERIT} ${KEY} VALUE)
+    endif()
+
+    # Return value
+    set(${OUTPUT_VARIABLE} "${VALUE}" PARENT_SCOPE)
+endfunction()
