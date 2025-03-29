0c5bca548ff144532ca2faa47ddeae29ac6cc7a9
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-30 00:56:00 +0300

    change patches

--- transpiler/cmake/ANTLR4/ExternalAntlr4Cpp.cmake
+++ transpiler/cmake/ANTLR4/ExternalAntlr4Cpp.cmake
@@ -112,8 +112,8 @@ else()
       # <CUSTOM MODIFICATIONS
       PATCH_COMMAND
         ${GIT_PATCH_EXECUTABLE} ${ANTLR4_ROOT}/runtime/Cpp/runtime/src/atn/ProfilingATNSimulator.cpp < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__ProfilingATNSimulator.cpp.diff
-        #COMMAND ${GIT_PATCH_EXECUTABLE} ${ANTLR4_ROOT}/runtime/Cpp/runtime/antlrcpp.xcodeproj/project.pbxproj < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__project.pbxproj.diff
-        COMMAND ${GIT_PATCH_EXECUTABLE} ${ANTLR4_ROOT}/runtime/Cpp/runtime/CMakeLists.txt < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__CMakeLists.txt.diff
+        COMMAND ${GIT_PATCH_EXECUTABLE} ${ANTLR4_ROOT}/runtime/Cpp/runtime/antlrcpp.xcodeproj/project.pbxproj < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__project.pbxproj.diff
+        #COMMAND ${GIT_PATCH_EXECUTABLE} ${ANTLR4_ROOT}/runtime/Cpp/runtime/CMakeLists.txt < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__CMakeLists.txt.diff
       # CUSTOM MODIFICATIONS>
       BUILD_COMMAND ""
       BUILD_IN_SOURCE 1
