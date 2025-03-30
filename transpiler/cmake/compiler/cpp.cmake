c7d8f96708455c912a540e4b69cdfac4b679aeba
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-30 04:47:07 +0300

    Add warning flags

--- transpiler/cmake/compiler/cpp.cmake
+++ transpiler/cmake/compiler/cpp.cmake
@@ -1,6 +1,3 @@
-# Force PIC
-set(CMAKE_POSITION_INDEPENDENT_CODE ON)
-
 # Check for C++17
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
@@ -8,8 +5,55 @@ include(CheckCXXCompilerFlag)
 CHECK_CXX_COMPILER_FLAG("-std=c++17" CXX_17_FLAG)
 CHECK_CXX_COMPILER_FLAG("-std:c++17" CXX_17_FLAG_MSVC)
 if(NOT (CXX_17_FLAG OR CXX_17_FLAG_MSVC))
-    message(FATAL_ERROR "[DEPENDENCIES] C++17 is not supported by the compiler. Please use a compiler that supports C++17.")
+    message(FATAL_ERROR "[C++] C++17 is not supported by the compiler. Please use a compiler that supports C++17.")
 endif()
+
+# Force PIC
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+# Warning flags (only for internal targets)
+function(add_internal_target_cxx_flags TARGET)
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+        # GCC or Clang
+        target_compile_options(${TARGET} PRIVATE
+            -Wall
+            -Wshadow
+            -Wpointer-arith
+            -Wcast-align
+            -Wstrict-overflow=4
+            -Wwrite-strings
+            -Wcast-qual
+            -Wconversion
+            -Wunreachable-code
+        )
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        # MSVC
+        target_compile_options(${TARGET} PRIVATE
+            /W4
+
+            /weC4456 # Declaration of identifier hides previous local declaration.
+            /weC4457 # Declaration of member hides previous local declaration.
+            /weC4458 # Declaration of identifier hides class member.
+            /weC4459 # Declaration of formal parameter shadows global declaration.
+
+            /weC4826 # Conversion from type 'type1' to type 'type2' requires reinterpretation of object representation.
+
+            /weC4230 # nonstandard extension used: string literal used for array initialization.
+            /weC4244 # conversion from 'const char [N]' to 'char *', possible loss of data.
+
+            /weC4180 # qualifier applied to function type has no meaning; ignored.
+            /weC4190 # 'identifier' has C-linkage specified, but returns UDT 'type' which is not C-linkage-compatible.
+
+            /weC4244 # conversion from 'type1' to 'type2', possible loss of data.
+            /weC4242 # conversion from 'type1' to 'type2', possible loss of data.
+
+            /weC4702 # unreachable code.
+        )
+    else()
+        # Other compilers (optional)
+        message(FATAL_ERROR "[C++] Compiler ${CMAKE_CXX_COMPILER_ID} not recognized. Can't set the proper warning flags.")
+    endif()
+endfunction()
 
 # Treat compiler warnings as errors
 set(CMAKE_COMPILE_WARNING_AS_ERROR ON)
