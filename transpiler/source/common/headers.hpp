b53fa24c35872c25af4f4658d398d779d34de609
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-03-30 03:36:06 +0300

    Add a WASM-related note

--- transpiler/source/common/headers.hpp
+++ transpiler/source/common/headers.hpp
@@ -25,6 +25,14 @@
     #include <unistd.h>
 #elif __APPLE__ // macOS (and other Apple platforms)
     #include <mach/mach_time.h>
+//#elif __EMSCRIPTEN__ // WASM (maybe? It'd be useful but it requires a lot of effort to implement)
+//    #include <emscripten.h>
 #else
   // ???
 #endif
+
+// If you decide to support WASM, expose library members like this:
+//#if __EMSCRIPTEN__ // WASM
+//#   define FRANKIE_BASE_LIB "C" EMSCRIPTEN_KEEPALIVE
+//#endif
+// Then you could compile the dynamic libraries into WASM side modules!
