8a184b761c7eec1a28ee68a04607688f4956900f
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-04-01 02:34:24 +0300

    Update `Comms::CLI::finalize`

--- transpiler/source/comms/CLI/finalize.cpp
+++ transpiler/source/comms/CLI/finalize.cpp
@@ -6,6 +6,7 @@
 #include "finalize.hpp"
 
 // Comms headers
+#include "../comms.hpp"
 #include "basic.hpp"
 
 // Base headers
@@ -20,22 +21,53 @@ namespace Comms {
                 return;
             }
 
+            // Keep track of the program's status
+            std::string status;
+            if (Comms::ProcessReport::programStatus == 0) {
+                status = "Success";
+            } else {
+                status = "Failure";
+            }
+
+            // Attach other status strings
+            int warnings = Statistics::warningReports;
+            int errors = Statistics::criticalReports + Statistics::fatalReports;
+
+            if (errors > 0 && warnings > 0) {
+                status.append(", with warnings & errors!");
+            } else if (errors > 0) {
+                status.append(", with errors!");
+            } else if (warnings > 0) {
+                status.append(", with warnings!");
+            } else {
+                status.append("!");
+            }
+
             /**
              * Failure!
              * Success, with warnings!
-             * Success, with non-fatal errors!
+             * Success, with errors!
+             * Success, with warnings & errors!
              * Success!
              * Success, with warnings!
-             * Success, with non-fatal errors!
+             * Success, with errors!
              * 
              *                 |\__/,|   (`\
              *               _.|o o  |_   ) )
-             * ----------- -(((---(((--------  -----------------------
+             * -------------(((---(((---------------------------------
              * 
              * 0 action(s), 0 warning(s), 0 error(s), 0 fatal error(s)
             **/
-            // Print initial console prompt
-            std::cout << std::endl << format("Done!", Color::light_sea_green) << std::endl;
+            // Print summary
+            std::cout << std::endl << std::endl << format(status, Color::light_sea_green) << std::endl << std::endl;
+            std::cout << format("                |\\__/,|   (`\\", Color::golden_rod) << std::endl;
+            std::cout << format("              _.|o o  |_   ) )", Color::golden_rod) << std::endl;
+            std::cout << format("-------------", Color::light_sea_green) << format("(((", Color::golden_rod) << format("---", Color::light_sea_green)
+                << format("(((", Color::golden_rod) << format("---------------------------------", Color::light_sea_green) << std::endl << std::endl;
+            std::cout << format(std::to_string(Statistics::actionReports), Color::sea_green) << format(" action(s), ", Color::light_sea_green)
+                << format(std::to_string(Statistics::warningReports), Color::golden_rod) << format(" warning(s), ", Color::light_sea_green)
+                << format(std::to_string(Statistics::criticalReports), Color::crimson) << format(" error(s), ", Color::light_sea_green)
+                << format(std::to_string(Statistics::fatalReports), Color::crimson) << format(" fatal error(s)", Color::light_sea_green) << std::endl;
         }
     }
 }
