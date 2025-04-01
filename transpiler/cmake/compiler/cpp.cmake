d1d32635fd35e76ded7b672dbe47fda35d4dd9ac
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-04-02 01:43:07 +0300

    Undo last change

--- transpiler/cmake/compiler/cpp.cmake
+++ transpiler/cmake/compiler/cpp.cmake
@@ -58,7 +58,6 @@ function(add_internal_target_cxx_flags TARGET IS_LESS_RESTRICTIVE)
         if(NOT IS_LESS_RESTRICTIVE)
             target_compile_options(${TARGET} PRIVATE
                 -Wshadow
-                -Wunused-parameter
             )
         endif()
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
@@ -95,8 +94,6 @@ function(add_internal_target_cxx_flags TARGET IS_LESS_RESTRICTIVE)
             target_compile_options(${TARGET} PRIVATE
                 /we6244 # local declaration of <variable> hides previous declaration at <line> of <file>
                 /we6246 # Local declaration of <variable> hides declaration of same name in outer scope    
-
-                /we4100 # unreferenced formal parameter (Enable)
             )
         endif()
     else()
