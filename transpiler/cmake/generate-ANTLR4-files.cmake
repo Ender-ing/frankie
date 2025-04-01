623a4062052892960229321ebd7fd4d51696e3b3
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-04-02 00:21:40 +0300

    Remove extra whitespace

--- transpiler/cmake/generate-ANTLR4-files.cmake
+++ transpiler/cmake/generate-ANTLR4-files.cmake
@@ -34,11 +34,11 @@ find_package(ANTLR REQUIRED)
 
 # Call macro to add lexer and grammar to your build dependencies.
 antlr_target(FrankieGrammarLexer ${FRANKIE_ANTLR4_LEXER_PATH} LEXER
-             PACKAGE frankie_parser)
+            PACKAGE frankie_parser)
 antlr_target(FrankieGrammarParser ${FRANKIE_ANTLR4_PARSER_PATH} PARSER
-             PACKAGE frankie_parser
-             DEPENDS_ANTLR FrankieGrammarLexer
-             COMPILE_FLAGS -lib ${ANTLR_FrankieGrammarLexer_OUTPUT_DIR})
+            PACKAGE frankie_parser
+            DEPENDS_ANTLR FrankieGrammarLexer
+            COMPILE_FLAGS -lib ${ANTLR_FrankieGrammarLexer_OUTPUT_DIR})
 
 # include generated files in project environment
 include_directories(${ANTLR_FrankieGrammarLexer_OUTPUT_DIR})
