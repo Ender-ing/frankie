f64c60756356364b021807572d5690d525f54864
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-04-02 08:58:09 +0300

    ...linking issues on Windows and macOS

--- transpiler/source/parser/listeners/errors.hpp
+++ transpiler/source/parser/listeners/errors.hpp
@@ -17,7 +17,7 @@ namespace Parser {
         extern FRANKIE_PARSER_API bool errorsDetected;
 
         // Listen for syntax-related errors
-        class ErrorListener : public antlr4::BaseErrorListener {
+        class FRANKIE_PARSER_API ErrorListener : public antlr4::BaseErrorListener {
             public:
                 void syntaxError(antlr4::Recognizer *recognizer, antlr4::Token *offendingSymbol, size_t line,
                     size_t charPositionInLine, const std::string &msg, std::exception_ptr e) override ;
@@ -25,13 +25,13 @@ namespace Parser {
                     size_t stopIndex, bool exact, const antlrcpp::BitSet &ambigAlts, antlr4::atn::ATNConfigSet *configs)
                     override ;
                 void reportAttemptingFullContext(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa,
-                        size_t startIndex, size_t stopIndex, const antlrcpp::BitSet &conflictingAlts,
-                        antlr4::atn::ATNConfigSet *configs) override ;
+                    size_t startIndex, size_t stopIndex, const antlrcpp::BitSet &conflictingAlts,
+                    antlr4::atn::ATNConfigSet *configs) override ;
                 void reportContextSensitivity(antlr4::Parser *recognizer, const antlr4::dfa::DFA &dfa,
-                        size_t startIndex, size_t stopIndex, size_t prediction,
-                        antlr4::atn::ATNConfigSet *configs) override ;
+                    size_t startIndex, size_t stopIndex, size_t prediction,
+                    antlr4::atn::ATNConfigSet *configs) override ;
 
-                std::string stage = ""; // "Lexer" or "Parser"
+                std::string stage; // "Lexer" or "Parser"
             private:
         };
     }
