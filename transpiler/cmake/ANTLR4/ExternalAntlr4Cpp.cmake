538c92d0b1eb4914915eb9d861f5297151c809b8
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-04-07 07:39:08 +0300

    Make the build command reusable

--- transpiler/cmake/ANTLR4/ExternalAntlr4Cpp.cmake
+++ transpiler/cmake/ANTLR4/ExternalAntlr4Cpp.cmake
@@ -111,9 +111,21 @@ else()
       DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
       # <CUSTOM MODIFICATIONS
       PATCH_COMMAND
-        COMMAND ${GIT_PATCH_EXECUTABLE} ${ANTLR4_ROOT}/runtime/Cpp/runtime/CMakeLists.txt < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__CMakeLists.txt.diff
-        COMMAND ${GIT_PATCH_EXECUTABLE} ${ANTLR4_ROOT}/runtime/Cpp/runtime/src/atn/ProfilingATNSimulator.cpp < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__ProfilingATNSimulator.cpp.diff
-        COMMAND ${GIT_PATCH_EXECUTABLE} ${ANTLR4_ROOT}/runtime/Cpp/runtime/antlrcpp.xcodeproj/project.pbxproj < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__project.pbxproj.diff
+        COMMAND ${ANTLR4_PATCH_COMMAND}
+          --forward --force --fuzz=0
+            ${ANTLR4_ROOT}/runtime/Cpp/runtime/CMakeLists.txt
+            < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__CMakeLists.txt.diff
+          || true
+        COMMAND ${ANTLR4_PATCH_COMMAND}
+          --forward --force --fuzz=0
+            ${ANTLR4_ROOT}/runtime/Cpp/runtime/src/atn/ProfilingATNSimulator.cpp
+            < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__ProfilingATNSimulator.cpp.diff
+          || true
+        COMMAND ${ANTLR4_PATCH_COMMAND}
+          --forward --force --fuzz=0
+            ${ANTLR4_ROOT}/runtime/Cpp/runtime/antlrcpp.xcodeproj/project.pbxproj
+            < ${FRANKIE_CMAKE_DIR}/ANTLR4/patches/antlr4_runtime_patch__project.pbxproj.diff
+          || true
       # CUSTOM MODIFICATIONS>
       BUILD_COMMAND ""
       BUILD_IN_SOURCE 1
